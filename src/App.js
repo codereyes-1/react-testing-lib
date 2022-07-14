cd908f75a412e04088f9c9665c2a2bc30abb39f6
codereyes-1 <80291124+codereyes-1@users.noreply.github.com>
2022-07-13 22:45:01 -0500

    +components, tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import todo from "./components/todo";
+import Todo from "./components/todo";
 
 function App() {
+  const todos = [
+    { id: 1, title: "wash dishes", completed: false },
+    { id: 2, title: "label packages", completed: true },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {todos.map((todo) => {
+        return <Todo todo={todo} />;
+      })}
     </div>
   );
 }
---
+++ src/components/__tests__/todo.tests.js
@@ -0,0 +1,39 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import Todo from "../todo";
+
+afterEach(() => {
+  cleanup();
+});
+
+test("should render non-completed to do component", () => {
+  const todo = { id: 1, title: "wash dishes", completed: false };
+  render(<Todo todo={todo} />);
+  const todoElement = screen.getByTestId("todo-1");
+  expect(todoElement).toBeInTheDocument();
+  expect(todoElement).toHaveTextContent("wash dishes");
+  expect(todoElement).not.toContainHTML("<strike>");
+});
+
+test("should render completed to do component", () => {
+  const todo = { id: 2, title: "label packages", completed: true };
+  render(<Todo todo={todo} />);
+  const todoElement = screen.getByTestId("todo-2");
+  expect(todoElement).toBeInTheDocument();
+  expect(todoElement).toHaveTextContent("label packages");
+  expect(todoElement).toContainHTML("</strike>");
+});
+
+test("matches snapshot", () => {
+  const todo = { id: 1, title: "wash dishes", completed: false };
+  const tree = renderer.create(<Todo todo={todo} />).toJSON();
+  expect(tree).toMatchSnapshot();
+  console.log(tree);
+});
+
+// test("matches snapshot", () => {
+//   const todo = { id: 2, title: "label packages", completed: true };
+//   const tree = renderer.create(<Todo todo={todo} />).toJSON();
+//   expect(tree).toMatchInlineSnapshot();
+//   console.log(tree);
+// });
